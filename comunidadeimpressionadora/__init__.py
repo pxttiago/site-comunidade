9fd0159839d16c0ffe322e017aad15e8e292a1ea
Tiago S. Peixoto <88716217+pxttiago@users.noreply.github.com>
2023-12-13 14:41:48 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -6,7 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)  # criação do site/app
 
-app.config['SECRET_KEY'] = 'bfca39860e03668bb8d3e66eb1fb09e9'  # configuração da
+app.config['SECRET_KEY'] = 'bfca39860e03668bb8d3e66eb1fb09e9'  # configuração da secret key
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'  # configuração do banco de dados
 
 database = SQLAlchemy(app)  # cria o banco de dados
